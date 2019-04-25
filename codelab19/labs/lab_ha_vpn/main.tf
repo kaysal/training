fa7e97a0611152c00e9e160cc0d9fffef491c111
kaysal <salawu@google.com>
2019-04-25 06:45:41 +0100

    added vpc onprem

--- codelab19/labs/lab_ha_vpn/main.tf
+++ codelab19/labs/lab_ha_vpn/main.tf
@@ -74,3 +74,66 @@ module "vpc_demo_vm_10_1_1" {
   subnetwork_project      = "${var.project_id}"
   subnetwork              = "${module.vpc_demo.subnets_self_links[0]}"
 }
+
+
+#============================================
+# VPC On-prem Configuration
+#============================================
+
+locals {
+  vpc_onprem_subnet_10_128_1 = "${local.prefix}vpc-onprem-subnet-10-128-1"
+}
+
+module "vpc_onprem" {
+  source  = "terraform-google-modules/network/google"
+  version = "0.6.0"
+  project_id   = "${var.project_id}"
+  network_name = "${local.prefix}vpc-onprem"
+  routing_mode = "GLOBAL"
+
+  subnets = [
+    {
+      subnet_name           = "${local.vpc_onprem_subnet_10_128_1}"
+      subnet_ip             = "10.128.1.0/24"
+      subnet_region         = "us-central1"
+      subnet_private_access = false
+      subnet_flow_logs      = false
+    },
+  ]
+
+  secondary_ranges = {
+    "${local.vpc_onprem_subnet_10_128_1}" = []
+  }
+}
+
+resource "google_compute_firewall" "vpc_onprem_fw_rule" {
+  provider    = "google-beta"
+  name        = "${local.prefix}vpc-onprem-fw-rule"
+  description = "VPC Onprem FW rules"
+  network     = "${module.vpc_onprem.network_self_link}"
+
+  allow {
+    protocol = "tcp"
+  }
+  allow {
+    protocol = "udp"
+  }
+  allow {
+    protocol = "icmp"
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+# VM Instance
+#-----------------------------------
+module "vpc_onprem_vm" {
+  source                  = "../../modules/gce"
+  project                 = "${var.project_id}"
+  name                    = "${local.prefix}vpc-onprem-vm"
+  machine_type            = "${local.machine_type}"
+  zone                    = "us-central1-a"
+  metadata_startup_script = "${file("scripts/startup.sh")}"
+  image                   = "${local.image}"
+  subnetwork_project      = "${var.project_id}"
+  subnetwork              = "${module.vpc_onprem.subnets_self_links[0]}"
+}
