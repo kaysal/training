8d6c278aa93af8535d56b16cbc39d6193429c7c9
kaysal <salawu@google.com>
2019-05-07 20:08:52 +0100

    added error checking to Terraform apply

--- codelab19/init.sh
+++ codelab19/init.sh
@@ -59,11 +59,20 @@ tf_apply() {
   printf "\nRunning ${green}${bold}terraform plan${reset} in directory ${magenta}${bold}$1${reset}...\n"
   terraform plan -input=false -out tfplan -var project_id=$project_id
   printf "Running ${green}${bold}terraform apply${reset} in directory ${magenta}${bold}$1${reset}...\n"
+
   terraform apply -input=false tfplan
-  if [ -f tfplan ]; then
+  if [ $? -eq 0 ]; then
+    printf "\n${bold}${green}$2 deployed successfully!${reset}\n"
     rm tfplan
+    popd > /dev/null
+    touch .tmp && echo $2 > .tmp
+  else
+    printf "\n${bold}${red}Terraform error while deploying $2 Lab !!!${reset}\n"
+    printf "\nUse the troubleshooting guide to resolve the error code.${reset}\n"
+    rm tfplan
+    popd > /dev/null
+    touch .tmp && echo $2 > .tmp
   fi
-  popd > /dev/null
 }
 
 export TF_WARN_OUTPUT_ERRORS=1
@@ -74,14 +83,15 @@ if [[ -s .tmp ]]; then
   printf "\n${green}${bold}$LAB${reset} lab is already deployed!\n"
   read -p "Re-deploy ${green}${bold}$LAB${reset} lab? ( Y/N  y/n  yes/no ): "
   if [[ ! $REPLY =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    printf "To deploy a new lab, you must remove the existing lab"
     return
   else
-    time tf_apply "labs/${LAB}/"
+    time tf_apply "labs/${LAB}/" "${LAB}"
+    touch .tmp && echo ${LAB} > .tmp
   fi
   printf "\n${bold}GOOGLE_PROJECT${reset} variable = ${green}${bold}[$GOOGLE_PROJECT]${reset}\n"
   printf "${bold}TF_VAR_project_id${reset} variable = ${green}${bold}[$TF_VAR_project_id]${reset}\n"
 else
   init
-  time tf_apply "labs/${LAB}/"
-  touch .tmp && echo ${LAB} > .tmp
+  time tf_apply "labs/${LAB}/" "${LAB}"
 fi
