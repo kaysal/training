e5f6e8e6c38467c81966ed6e29966f0c7f745aaf
kaysal <salawu@google.com>
2020-01-28 19:38:14 +0000

    Terraform 0.12 syntax update

--- codelab19v3/modules/gce-private/main.tf
+++ codelab19v3/modules/gce-private/main.tf
@@ -13,24 +13,24 @@
 # limitations under the License.
 
 resource "google_compute_instance" "instance" {
-  project      = "${var.project}"
-  name         = "${var.name}"
-  machine_type = "${var.machine_type}"
-  zone         = "${var.zone}"
-  tags         = "${var.tags}"
+  project      = var.project
+  name         = var.name
+  machine_type = var.machine_type
+  zone         = var.zone
+  tags         = var.tags
 
   #allow_stopping_for_update = true
-  metadata_startup_script = "${var.metadata_startup_script}"
+  metadata_startup_script = var.metadata_startup_script
 
   boot_disk {
     initialize_params {
-      image = "${var.image}"
+      image = var.image
     }
   }
 
   network_interface {
-    subnetwork_project = "${var.subnetwork_project}"
-    subnetwork         = "${var.subnetwork}"
+    subnetwork_project = var.subnetwork_project
+    subnetwork         = var.subnetwork
   }
 
   service_account {
